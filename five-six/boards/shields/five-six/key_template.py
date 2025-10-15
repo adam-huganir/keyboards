87fe41bdead4a4df2a34572081be0483db30bb95
Adam Huganir <adam@huganir.com>
2025-10-14 21:13:41 -0400

    python script looks good

--- five-six/boards/shields/five-six/key_template.py
+++ five-six/boards/shields/five-six/key_template.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python3
 
 from enum import StrEnum
+import sys
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--debug", action="store_true", help="Enable debug output")
+parser.add_argument("--width", "-w", type=int, default=15, help="Width of each key")
+parser.add_argument(
+    "--comment", "-c", type=str, default="", help="Comment to add to the beginning drawn line"
+)
+parser.add_argument("--spacing", "-s", type=int, default=1, help="Add newline between keys")
+args = parser.parse_args()
 
 
 # Reference Layout
@@ -31,6 +42,7 @@ class TableChars(StrEnum):
     BR = "┘"
     BM = "┴"
     KEY = "─"
+    EMPTY = " "
 
 
 layout_as_lines = [
@@ -46,7 +58,7 @@ max_width = max(sum(x) for x in layout_as_lines)
 
 # Convert layout to character grid, handling fractional offsets
 filled_layout = []
-matrix_scale = 3
+matrix_scale = args.width
 for offset, count in layout_as_lines:
     # Calculate spaces needed (multiply by 2 to handle 0.5 increments)
     spaces = int(offset * (matrix_scale + 1))
@@ -78,32 +90,28 @@ def coord_surrounds(r, c):
     return above, below, left, right
 
 
-key_width = 15
-for row_idx, row in enumerate(filled_layout):
-    line = "|"
-    for col_idx, cell in enumerate(row):
-        preds = coord_surrounds(row_idx, col_idx)
-        line += "".join([x or " " for x in preds])
-        line += "|"
-    print(line)
-
+final_layout = []
+debug = False
 for row_idx, row in enumerate(filled_layout):
     line = ""
     for col_idx, cell in enumerate(row):
         preds = coord_surrounds(row_idx, col_idx)
         above, below, left, right = preds
-
+        error_message = f"Invalid at {row_idx}, {col_idx} {cell!r} {''.join([str(x) for x in preds])!r}"
         match above, below, left, right:
             case (CC.JUNC, CC.JUNC, l, r):
                 match l, r:
                     case (CC.KEY, CC.KEY):
-                        line += TableChars.MM
+                        if cell == CC.JUNC:
+                            line += TableChars.MM
+                        else:
+                            line += TableChars.TM
                     case (CC.KEY, CC.EMPTY):
                         line += TableChars.MR
                     case (CC.EMPTY, CC.KEY):
                         line += TableChars.ML
                     case _:
-                        raise Exception(f"Invalid surround {preds}")
+                        raise Exception(error_message)
             case (CC.JUNC, CC.EMPTY, l, r):
                 match l, r:
                     case (CC.KEY, CC.KEY):
@@ -114,11 +122,11 @@ for row_idx, row in enumerate(filled_layout):
                     case (CC.EMPTY, CC.JUNC) | (CC.EMPTY, CC.KEY):
                         line += TableChars.BL
                     case (CC.JUNC, CC.EMPTY) | (CC.KEY, CC.EMPTY):
-                        line += TableChars.ML
+                        line += TableChars.BR
                     case (CC.EMPTY, CC.EMPTY):
-                        line += " "
+                        line += TableChars.EMPTY
                     case _:
-                        raise Exception(f"Invalid surround {preds}")
+                        raise Exception(error_message)
             case (CC.EMPTY, CC.JUNC, l, r):
                 match l, r:
                     case (CC.EMPTY, CC.KEY):
@@ -127,31 +135,96 @@ for row_idx, row in enumerate(filled_layout):
                         line += TableChars.TM
                     case (CC.KEY, CC.EMPTY):
                         line += TableChars.TR
+                    case (CC.EMPTY, CC.EMPTY):
+                        line += TableChars.TR
+                    case _:
+                        raise Exception(error_message)
             case (CC.EMPTY, CC.EMPTY, l, r):
                 match l, r:
-                    case (CC.EMPTY, CC.EMPTY) | (CC.EMPTY, CC.JUNC) | (CC.JUNC, CC.EMPTY):
-                        line += " "
+                    case (CC.EMPTY, CC.JUNC) | (CC.JUNC, CC.EMPTY):
+                        line += TableChars.EMPTY
+                    case (CC.JUNC, CC.EMPTY):
+                        line += TableChars.EMPTY
+                    case (CC.EMPTY, CC.EMPTY):
+                        line += TableChars.EMPTY
                     case (CC.EMPTY, CC.KEY) | (CC.KEY, CC.EMPTY):
                         line += TableChars.BR
                     case (CC.JUNC, CC.KEY) | (CC.KEY, CC.JUNC):
                         line += TableChars.KEY
+                    case (CC.KEY, CC.KEY):
+                        line += TableChars.KEY
                     case _:
-                        raise Exception(f"Invalid surround {preds}")
-            case (CC.EMPTY, CC.KEY, _, _):
-                line += TableChars.KEY
-            case (CC.KEY, CC.JUNC, CC.KEY, CC.KEY):
-                line += TableChars.TM
-            case (CC.KEY, CC.EMPTY, CC.KEY, _):
-                line += TableChars.KEY
-            case (CC.KEY, CC.EMPTY, _, CC.KEY):
-                line += TableChars.KEY
-            case (CC.KEY, CC.EMPTY, CC.EMPTY, CC.EMPTY):
-                line += " "
+                        raise Exception(error_message)
+            case (CC.EMPTY, CC.KEY, l, r):
+                match l, r:
+                    case (CC.JUNC, CC.KEY) | (CC.KEY, CC.JUNC) | (CC.KEY, CC.KEY):
+                        line += TableChars.KEY
+                    case (
+                        (CC.EMPTY, CC.JUNC) | (CC.JUNC, CC.EMPTY) | (CC.EMPTY, CC.EMPTY)
+                    ):
+                        line += TableChars.KEY
+                    case (CC.EMPTY, CC.KEY) | (CC.KEY, CC.EMPTY):
+                        line += TableChars.BM
+                    case _:
+                        raise Exception(error_message)
+            case (CC.KEY, CC.EMPTY, l, r):
+                match l, r:
+                    case (CC.JUNC, CC.KEY) | (CC.KEY, CC.JUNC) | (CC.KEY, CC.KEY):
+                        if cell == CC.JUNC:
+                            line += TableChars.BM
+                        else:
+                            line += TableChars.KEY
+                    case (
+                        (CC.EMPTY, CC.JUNC) | (CC.JUNC, CC.EMPTY) | (CC.EMPTY, CC.EMPTY)
+                    ):
+                        line += TableChars.EMPTY
+                    case (CC.EMPTY, CC.KEY):
+                        line += TableChars.BL
+                    case _:
+                        raise Exception(error_message)
+            case (CC.KEY, CC.JUNC, l, r):
+                match l, r:
+                    case (CC.KEY, CC.KEY):
+                        line += TableChars.TM
+                    case (CC.KEY, CC.EMPTY):
+                        line += "F"
+                    case (CC.EMPTY, CC.KEY):
+                        line += "G"
+                    case _:
+                        raise Exception(error_message)
+            case (CC.JUNC, CC.KEY, l, r):
+                match l, r:
+                    case (CC.KEY, CC.KEY):
+                        line += TableChars.BM
+                    case (CC.KEY, CC.EMPTY):
+                        line += TableChars.BM
+                    case _:
+                        raise Exception(error_message)
             case (CC.KEY, CC.KEY, _, _):
                 line += TableChars.KEY
-            case (CC.EMPTY, CC.EMPTY, _, _):
-                line += " "
             case _:
-                line += "."
-                # raise Exception(f"Invalid surround {preds}")
+                raise Exception(f"Invalid surround {preds}")
+        if debug:
+            print(line[-1], end="")
+    if debug:
+        print()
+    final_layout.append(line)
+
+for line in final_layout:
+    line = args.comment + " " + line
+    for _ in range(args.spacing):
+        line += "\n"
     print(line)
+
+for line in final_layout:
+    rev = args.comment + " "
+    for c in line[::-1]:
+        n = TableChars(c).name
+        if "L" in n:
+            n = n.replace("L", "R")
+        elif "R" in n:
+            n = n.replace("R", "L")
+        rev += TableChars[n]
+    for _ in range(args.spacing):
+        rev += "\n"
+    print(rev)
